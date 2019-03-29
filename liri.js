bccf3f563e54655a0e5cdfee1c0fa16a08bb83a6
Blake Bennett <blakebennettc@gmail.com>
2019-03-29 15:55:30 -0500

    dependencies added

--- liri.js
+++ liri.js
@@ -1,7 +1,15 @@
+//this loads the spotify api key from the .env and uses Spotify api
 require("dotenv").config();
 var keys = require("./keys.js");
-
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
+
+//other dependencies
+var axios = require("axios"); //for OMDB and Bands in Town api calls
+var moment = require("moment"); //for time and date parsing
+
+
+console.log(spotify);
 
 //commands
 //concert-this
